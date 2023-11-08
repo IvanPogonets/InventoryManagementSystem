f1424fe90d81ebdd2a4b0ae20ecdd7427f96aad0
JK <jk.4work@gmail.com>
2023-11-09 00:52:25 +0200

    added methods

--- src/com/InventoryManagementSystem/Interfaces/Item.java
+++ src/com/InventoryManagementSystem/Interfaces/Item.java
@@ -3,5 +3,4 @@ package com.InventoryManagementSystem.Interfaces;
 public interface Item {
     String getItemDetails();
 
-    void displayDescription();
 }
--- src/com/InventoryManagementSystem/items/InventoryItem.java
+++ src/com/InventoryManagementSystem/items/InventoryItem.java
@@ -1,10 +1,10 @@
 package com.InventoryManagementSystem.items;
 
-public class InventoryItem extends AbstractItem{
+public class InventoryItem extends AbstractItem {
     static long idCount = 0;
     long id;
     String name;
-    boolean sellable;
+    boolean sellable = true;
     double quantity;
     String unitsOfMeasurement;
     double price;
@@ -14,14 +14,25 @@ public class InventoryItem extends AbstractItem{
     public double getPrice() {
         return price;
     }
+
     public long getId() {
         return id;
     }
 
     @Override
     public String toString() {
-        return id + "," + name + "," +sellable + "," + quantity
+        return id + "," + name + "," + sellable + "," + quantity
                 + "," + unitsOfMeasurement + "," + price + "," + description;
+    }
+
+    public void setNewItem(String... data) {
+        idCount += 1;
+        this.id = idCount;
+        this.name = data[0];
+        this.quantity = Double.parseDouble(data[1]);
+        this.unitsOfMeasurement = data[2];
+        this.price = Double.parseDouble(data[3]);
+        this.description = data[4];
     }
 
     public void setQuantity(int quantity) {
@@ -29,21 +40,16 @@ public class InventoryItem extends AbstractItem{
     }
 
     public void decreaseQuantity(int quantity) {
+        this.quantity -= quantity;
     }
 
     public void setUnsellable() {
-    }
-
-    public void setBroken() {
+        this.sellable = false;
     }
 
     @Override
     public String getItemDetails() {
-        return description;
-    }
-
-    @Override
-    public void displayDescription() {
-
+        return name + ", " + quantity + "," + unitsOfMeasurement +
+                "," + price + "," + description;
     }
 }
